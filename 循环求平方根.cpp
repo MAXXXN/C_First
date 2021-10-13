daf90e335644af41ab1aa6bea1fca9dec71c3937
MAXXXN <90753853+MAXXXN@users.noreply.github.com>
2021-10-13 12:54:19 +0800

    Add files via upload

---
+++ 循环求平方根.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+float mysqrt(float n) {
+	float x = n;
+	float y = 0;
+	float ans = n/2;
+	while((ans*ans-n)>0.000001||(n-ans*ans)>0.000001) {
+		if (ans*ans > n+0.000001) {
+			x = ans;
+			ans = (x+y)/2;
+		}
+		else{
+			y = ans;
+			ans = (x+y)/2; 
+		}
+	}
+	return ans;
+}
+int main() {
+	float n;
+	scanf("%f",&n);
+	n=mysqrt(n);
+	printf("平方根为：%f",n); 
+	return 0;
+} 
